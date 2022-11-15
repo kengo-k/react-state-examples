995c948bc2806cc8cfacaf5c70d039ef19039e5f
kengo-k <kurobanekengo@gmail.com>
2022-11-15 22:10:20 +0900

    UserListPlain: ローディング導入

--- src/components/UserListPlain.tsx
+++ src/components/UserListPlain.tsx
@@ -5,13 +5,23 @@ import { Item } from '~/utils/types'
 
 const UserListPlain = () => {
   console.log('render start...')
+  const [loading, setLoading] = useState<boolean>(true)
   const [items, setItems] = useState<Item[]>([])
 
   useEffect(() => {
     fetchItems().then((data) => {
       setItems(data)
+      setLoading(false)
     })
   }, [])
+
+  if (loading) {
+    return (
+      <div className="flex justify-center">
+        <div className="animate-spin h-10 w-10 border-4 border-blue-500 rounded-full border-t-transparent"></div>
+      </div>
+    )
+  }
 
   return (
     <div className="overflow-x-auto relative shadow-md sm:rounded-lg my-4">
